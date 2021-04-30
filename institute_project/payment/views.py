8d27b7fe715c4d36c267819eb99b257ba64bcff6
aarichirag <patelarati1993@gmail.com>
2021-04-30 11:51:34 +0530

    pending fees updated in payment create view

--- institute_project/payment/views.py
+++ institute_project/payment/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render
 from django.views.generic import CreateView,ListView,UpdateView,DetailView,DeleteView
 from .models import payment_details
+from django.http import HttpResponseRedirect
 
 #Create your views here.
 
 class NewPayment(CreateView):
     model = payment_details
     fields = '__all__'
+
+    def form_valid(self, form):
+        self.object = form.save()
+        self.object.addmission.pending_fees=self.object.addmission.pending_fees - self.object.amount
+        self.object.addmission.save()
+        # do something with self.object
+        # remember the import: from django.http import HttpResponseRedirect
+        return HttpResponseRedirect(self.get_success_url())
+
 
 class ViewPayment(ListView):
     model = payment_details
